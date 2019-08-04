118c524fcb177e64a6b0a3da1c91186ea43cb1ec
Ezra Dowd <ezra.dowd@gmail.com>
2019-08-04 09:55:09 -0400

    Add comments and clean things up a bit

--- puritybot/bot.py
+++ puritybot/bot.py
@@ -12,16 +12,31 @@ import puritybot.model as model
 
 class PurityBot(object):
 
-    def __init__(self, harassment_cooldown: int, post_lookback: int, purity_threshold: int,
+    def __init__(self,
+                 harassment_cooldown: int,
+                 post_lookback: int,
+                 purity_threshold: int,
                  impure_subs: List[str]) -> None:
+        """
+        Create a new instance of the PurityBot
+        :param harassment_cooldown: the minimum threshold within which the bot will reply to the same users comments
+        :param post_lookback: the number of posts to include in the lookback period when making a bias determination
+        :param purity_threshold: the number of impure posts allowed before making a bias determination
+        :param impure_subs: the list of impure subreddits
+        """
         self._reddit = praw.Reddit(site_name="PurityBot")
-        self._db = model.Database()
+        self._db = model.Database("PurityBot")
         self._purity_cache = ExpiringDict(max_len=10000, max_age_seconds=harassment_cooldown)
         self._purity_threshold = purity_threshold
         self._impure_subs = impure_subs
         self._post_lookback = post_lookback
 
     def _count_impure_posts(self, user: Redditor) -> Dict[str, int]:
+        """
+        Count the number of impure posts for a given user and store it in the purity cache
+        :param user: the user to make a determination on
+        :return: the impurity dict
+        """
         counts = defaultdict(int)
         for submission in user.submissions.new(limit=self._post_lookback):
             if submission.subreddit.display_name in self._impure_subs:
@@ -30,6 +45,11 @@ class PurityBot(object):
         return counts
 
     def stream(self, subreddit: str) -> None:
+        """
+        Main run method for the bot, listens to a stream of posts coming from the given subreddit.  Replies to users
+        whose bias passes the impurity threshold.
+        :param subreddit: the subreddit to scan
+        """
         print(f"Beginning Chapo scan on {subreddit}")
         subreddit = self._reddit.subreddit(subreddit)
         for comment in subreddit.stream.comments():
@@ -43,10 +63,10 @@ class PurityBot(object):
                     is_impure = bias > self._purity_threshold
                     if is_impure:
                         print(
-                            f"{comment.author.name} failed purity test with {num_impure_posts}/{self._post_lookback} recent {sub_name} posts")
+                            f"{comment.author.name} failed purity test with {bias}/{self._post_lookback} recent {sub_name} posts")
                         warning_message = f"**Warning** it has been detected that this account may be a {sub_name} poster. Out " \
-                                          f"of {comment.author.name}'s last {self._post_lookback} posts, {bias} of them are on {sub_name}" \
-                                          f" posts. "
+                                          f"of {comment.author.name}'s last {self._post_lookback} posts, {bias} of them are on {sub_name}"
+                        print(warning_message)
                         # comment.reply(warning_message)
                 else:
                     sub_name, bias = determine_bias(num_impure_posts)
@@ -54,6 +74,11 @@ class PurityBot(object):
 
 
 def determine_bias(purity_dict: Dict[str, int]) -> Tuple[str, int]:
+    """
+    Attempts to determine which impure tag has the most bias
+    :param purity_dict: the purity dict
+    :return: a tuple containing the name of the biased subreddit and the degree of bias
+    """
     len_purity_dict = len(purity_dict)
     if len_purity_dict == 0:
         return '', 0
@@ -76,6 +101,11 @@ def determine_bias(purity_dict: Dict[str, int]) -> Tuple[str, int]:
 
 
 def determine_magnitude(x: int) -> int:
+    """
+    Determines the magnitude of a given integer
+    :param x: the integer
+    :return: the magnitude
+    """
     return int(math.log10(x))
 
 
--- puritybot/model.py
+++ puritybot/model.py
@@ -2,13 +2,16 @@ from sqlalchemy import create_engine, select
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, Boolean
 from sqlalchemy.engine import Connection
 
-_con_str = 'sqlite:///PurityBot.db'
+_con_str = 'sqlite:///{db_name}.db'
 
 
 class Database(object):
 
-    def __init__(self):
-        self._engine = create_engine(_con_str)
+    def __init__(self, db_name):
+        """
+        Create a new Database instance
+        """
+        self._engine = create_engine(_con_str.format(db_name=db_name))
         self._metadata = MetaData(self._engine)
         self._replied_to = Table('Replied_To', self._metadata,
                                  Column('Identifier', String, primary_key=True),
@@ -20,16 +23,31 @@ class Database(object):
         self._con = None
 
     def _get_connection(self) -> Connection:
+        """
+        Get or create a cached sqlite connection
+        :return: the connection object
+        """
         if self._con is None:
             self._con = self._engine.connect()
         return self._con
 
     def identifier_exists(self, identifier: str) -> bool:
-        stmt = select([self._replied_to.c.identifier]).where(self._replied_to.c.Identifier == identifier)
+        """
+        Determine if a comment or post identifier has been scanned
+        :param identifier: the identifier
+        :return: whether or not it has been scanned
+        """
+        stmt = select([self._replied_to.c.Identifier]).where(self._replied_to.c.Identifier == identifier)
         result = self._get_connection().execute(stmt)
         return len(list(result)) > 0
 
     def insert_new_record(self, identifier: str, poster: str, is_pure: bool) -> None:
+        """
+        Insert a new identifier record
+        :param identifier: the identifier
+        :param poster:     the poster
+        :param is_pure:    the poster purity flag
+        """
         ins = self._replied_to.insert().values(
             Identifier=identifier,
             Poster=poster,
